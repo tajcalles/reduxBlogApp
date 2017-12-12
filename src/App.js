35e4635c4c269de12604a59b02db31ea34fe96ce
Taj Calles <tajcalles@gmail.com>
2017-12-12 14:02:51 -0700

    app.js working

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Segment } from 'semantic-ui-react';
+
+const App = () => (
+  <Segment>
+    <Navbar />
+    <BlogForm />
+    <BlogList />
+  </Segment>
+)
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,2 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react';
